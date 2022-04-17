091a62cd95b4a6fc036ee996263a242fd33a24d7
Cera Thomason <cetxw3@gmail.com>
2022-04-17 13:40:05 -0500

    Finished chapter 2 data types studios.

---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+
+public class Circle {
+
+    public static Double getArea(Double radius) {
+        return 3.1416 * radius * radius;
+    }
+
+}
