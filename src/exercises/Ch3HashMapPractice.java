6836e4341f6e5f7564291e32b32589b92d96e0cc
Cera Thomason <cetxw3@gmail.com>
2022-04-19 13:19:20 -0500

    Finished chapter 3 exercises

--- src/exercises/Ch3HashMapPractice.java
+++ src/exercises/Ch3HashMapPractice.java
@@ -1,11 +1,40 @@
 package exercises;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Ch3HashMapPractice {
 
     public static void main(String[] args) {
 
+        HashMap<String, Integer> students = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String newStudent;
+
+        System.out.println("Enter your students (or ENTER to finish):");
+
+        //get student names and grades:
+        do {
+
+            System.out.print("Student: ");
+            newStudent = input.nextLine();
+
+            if(!newStudent.equals("")) {
+                System.out.print("ID Number: ");
+                Integer newID = input.nextInt();
+                students.put(newStudent, newID);
+
+                input.nextLine(); //read in the newline before looping back - this is what keeps the code going?
+            }
+
+        } while (!newStudent.equals(""));
+
+        //print class roster using HashMap loop
+        System.out.println("\nClass Roster:");
+        for(HashMap.Entry<String, Integer> student : students.entrySet()) {
+            System.out.println(student.getKey() + ", ID: " + student.getValue());
+        }
     }
 
 }
