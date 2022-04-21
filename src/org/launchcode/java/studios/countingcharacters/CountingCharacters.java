01be90936ab0ca6fba2b62d8c9ca0c2f2cb636d8
Cera Thomason <cetxw3@gmail.com>
2022-04-21 08:02:59 -0500

    Finished chapter 3 Studio

--- src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
 
+
+
 public class CountingCharacters {
 
     public static void main(String[] args) {
 
-        String hiddenFigures = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word or phrase to be counted:");
+        String wordPhrase = input.nextLine().toLowerCase();
+        input.close();
+
+        //String hiddenFigures = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
 
         HashMap<Character, Integer> characterCount = new HashMap<>();
 
-        for (char x : hiddenFigures.toCharArray()) {
+        for (char x : wordPhrase.toCharArray()) {
             int count;
 
             if (characterCount.containsKey(x)) {
